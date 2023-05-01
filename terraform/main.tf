04d6010a883eefe57486c9d78bab680cb75397ac
Simon Lynch <simon.lynch@hashicorp.com>
2023-05-01 16:47:15 +1000

    update var

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,6 +25,6 @@ resource "vault_policy" "policies" {
  resource "random_pet" "server" {
   keepers = {
     # Generate a new pet name each time we switch to a new AMI id
-    ami_id = "dog"
+    ami_id = "dog1"
   }
 }
