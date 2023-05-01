a5f0ce8c2afe2efafbc70b53461f15496f7254c9
Simon Lynch <simon.lynch@hashicorp.com>
2023-05-01 16:39:22 +1000

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,3 +22,9 @@ resource "vault_policy" "policies" {
 } */
  
  
+ resource "random_pet" "server" {
+  keepers = {
+    # Generate a new pet name each time we switch to a new AMI id
+    ami_id = "dog"
+  }
+}
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,3 +6,18 @@
 /* provider "vault" {
   # using environment variables
 } */
+
+
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
