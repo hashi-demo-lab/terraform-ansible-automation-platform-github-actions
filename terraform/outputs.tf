071bfea3b469530d649db8f381daf6cee92f34cd
Simon Lynch <simon.lynch@hashicorp.com>
2023-05-26 15:38:53 +1000

    add output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,3 +4,8 @@
   value       = vault_policy.policies
   description = "Sample helm values file that contains all of the configured paths that were created with this module. This should be used a reference and not a raw input to another object"
 } */
+
+
+output "pet_name" {
+  value = random_pet.server.id
+}
